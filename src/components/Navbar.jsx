c6107ca6c62d7d78847225235b8cb43c2c91878a
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-02 22:18:56 +0630

    Style navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,14 @@ import {PiForkKnifeLight} from 'react-icons/pi'
 
 const Navbar = () => {
     const [searchInput, setSearchInput] = useState('');
+
+    const handleInput = (e) => {
+        setSearchInput(e.target.value)
+    }
   return (
     <div className=' flex justify-around align-middle items-center py-5 bg-amber-500'>
         <PiForkKnifeLight className='text-3xl'/>
-        <input type="text" className='p-2 rounded-lg' readOnly value={searchInput}  placeholder='Search Your Favuorite Meal'/>
+        <input type="text" className='p-2 rounded-lg w-[250px]' onChange={handleInput} value={searchInput}  placeholder='Search Your Favuorite Meal'/>
     </div>
   )
 }
