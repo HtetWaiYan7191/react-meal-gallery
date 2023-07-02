ca23b86d6fa50615c94de4100b294f3fa40990ac
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-02 10:25:24 +0630

    Set up project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const App = () => {
+  return (
+    <div>
+      <h1 className=' text-emerald-500 text-4xl'>This is app component</h1>
+    </div>
+  )
+}
+
+export default App
