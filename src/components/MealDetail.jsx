b215160e26aeafeebb52b620d1de6a836dd4e45d
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-02 12:55:05 +0630

    Add detail page to show meal details

--- src/components/MealDetail.jsx
+++ src/components/MealDetail.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import '../style.css';
+import { useNavigate } from 'react-router-dom';
 
 const MealDetail = () => {
+    const navigate = useNavigate();
+
+    const goDetailPage = () => {
+        navigate('/details');
+    }
   return (
     <div className='absolute top-0 left-0 w-full bg-red-50/40 h-full flex justify-center align-middle items-center backgrop-filte card-layout'>
       <div className="detail-container">
-        <button className=' border rounded-lg bg-red-400 py-2 px-5'>Details</button>
+        <button className=' border rounded-lg bg-red-400 py-2 px-5' onClick={goDetailPage}>Details</button>
       </div>
     </div>
   )
