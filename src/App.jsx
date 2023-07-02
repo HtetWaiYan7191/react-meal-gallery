8c31c05294599db730d1e1eec9d549c2af9ef65e
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-02 12:55:14 +0630

    Add route for details page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import MealsContainer from './components/MealsContainer';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import Details from './components/Details';
 
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
     <Routes>
       <Route path='/' element = {<Home/>}></Route>
       <Route path='*' element={<Home/>}></Route>
+      <Route path='/details' element={<Details/>}></Route>
     </Routes>
     
   )
---
+++ src/components/Details.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Details = () => {
+  return (
+    <div>
+      This is detail Page
+    </div>
+  )
+}
+
+export default Details
