830d6136c047211d76017d183494f5df3b77b4ba
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-02 12:52:27 +0630

    update mealcard and meal detail

--- src/components/MealCard.jsx
+++ src/components/MealCard.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import MealDetail from './MealDetail';
+import '../style.css';
 
 const MealCard = ({meals}) => {
     return (
         <>
           {meals.map((meal, id) => {
-            return <div key={id} className=' relative'>
+            return <div key={id} className=' relative meal-card'>
                 <figure className=''>
                     <img src={meal.strMealThumb} alt="" className=' w-full object-cover' />
                     <figcaption>{meal.strMeal}</figcaption>
--- src/components/MealDetail.jsx
+++ src/components/MealDetail.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import '../style.css';
 
 const MealDetail = () => {
   return (
-    <div className='absolute top-0 left-0 w-full bg-red-50/50 h-full flex justify-center align-middle items-center backgrop-filte'>
+    <div className='absolute top-0 left-0 w-full bg-red-50/40 h-full flex justify-center align-middle items-center backgrop-filte card-layout'>
       <div className="detail-container">
         <button className=' border rounded-lg bg-red-400 py-2 px-5'>Details</button>
       </div>
