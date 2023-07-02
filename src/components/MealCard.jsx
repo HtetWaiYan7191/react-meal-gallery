f60cfc61cb1b6a139a3391e17470c5e697e90c91
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-02 22:38:10 +0630

    Enhance card design for better look

--- src/components/MealCard.jsx
+++ src/components/MealCard.jsx
@@ -6,10 +6,10 @@ const MealCard = ({meals}) => {
     return (
         <>
           {meals.map((meal, id) => {
-            return <div key={id} className=' relative meal-card'>
+            return <div key={id} className=' relative meal-card rounded-xl bg-pink-200 overflow-hidden '>
                 <figure className=''>
                     <img src={meal.strMealThumb} alt="" className=' w-full object-cover' />
-                    <figcaption>{meal.strMeal}</figcaption>
+                    <figcaption className='py-5 px-2 text-2xl font-lightbold font-mono'>{meal.strMeal}</figcaption>
                 </figure>
                 <MealDetail meal={meal}/>
             </div>;
