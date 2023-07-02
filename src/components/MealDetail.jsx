d93893001addab0569e9215cbcd2a5252c1a9e98
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-02 14:28:32 +0630

    Fetch detail api and pass the data to Details component

--- src/components/MealDetail.jsx
+++ src/components/MealDetail.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../style.css';
 import { useNavigate } from 'react-router-dom';
 
-const MealDetail = () => {
+const MealDetail = ({meal}) => {
+
+    useEffect (() => {
+        fetchDetails();
+    },[])
+
+    const fetchDetails = async () => {
+        const api = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${meal.idMeal}`);
+        const data = await api.json();
+        console.log(data)
+         meal = data;
+    }
     const navigate = useNavigate();
 
-    const goDetailPage = () => {
-        navigate('/details');
+    const goDetailPage = (e) => {
+        e.preventDefault();
+        navigate('/details',{state:{meal:meal}});
     }
   return (
     <div className='absolute top-0 left-0 w-full bg-red-50/40 h-full flex justify-center align-middle items-center backgrop-filte card-layout'>
